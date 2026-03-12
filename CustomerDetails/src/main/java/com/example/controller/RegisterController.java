b56045bcd044edc759cea038f816953a6b0f4f3a
Saichand Thokala <saichand1997.s5@gmail.com>
2026-03-12 21:31:58 +0530

    Sample Spring Boot

--- CustomerDetails/src/main/java/com/example/controller/RegisterController.java
+++ CustomerDetails/src/main/java/com/example/controller/RegisterController.java
@@ -1,5 +1,9 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +15,26 @@ import com.example.model.Customer;
 @RestController
 @RequestMapping("/company")
 public class RegisterController {
-	
+
+	private static final org.apache.logging.log4j.Logger logger = LogManager
+			.getLogger(RegisterController.class);
+
 	@GetMapping("/register")
-	public ResponseEntity<Customer> showRegister(){
-		
-		Customer cust= new Customer(1, "Sai", 13.3);
-		
-		HttpStatus status=HttpStatus.OK;
-		
-		return new ResponseEntity<Customer>(cust, status);
+	public ResponseEntity<List<Customer>> showRegister() {
+
+		logger.info("Helper Method is created and called");
+		// Customer cust = new Customer(1, "Sai", 13.3);
+
+		List<Customer> cust = new ArrayList<Customer>();
+		cust.add(new Customer(12, "Sai", 2382));
+		cust.add(new Customer(8, "Shafi", 821));
+		cust.add(new Customer(2, "Harshi", 29));
+
+		logger.info("Model class Object is created");
+
+		HttpStatus status = HttpStatus.OK;
+
+		return new ResponseEntity<List<Customer>>(cust, status);
 	}
 
 }
